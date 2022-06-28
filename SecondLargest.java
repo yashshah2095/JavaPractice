53fd973ca85160481452ebae8ff7f606dbb2b289
systems <systems@SYNPUNLT2206009.int.asurion.com>
2022-06-28 11:01:23 +0530

    Practice Problems

--- SecondLargest.java
+++ SecondLargest.java
@@ -1,6 +1,15 @@
+import java.util.Collections;
+import java.util.Set;
+import java.util.TreeSet;
+
 public class SecondLargest {
     public static void main(String[] args) {
         int arr1[]={1,2,3,4,5,2,1,10,20};
+        Set<Integer> set1=new TreeSet<>(Collections.reverseOrder());
+        for(int  el:arr1){
+            set1.add(el);
+        }
+        System.out.println("Second largest number is:"+ set1.toArray()[1]);
         System.out.println("Second largest number is:"+secondLargest(arr1));
     }
     public static int secondLargest(int[] num){
