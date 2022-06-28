70d3133021d1b1c77da57865e8abd3f5b41266c5
systems <systems@SYNPUNLT2206009.int.asurion.com>
2022-06-28 13:14:12 +0530

    Added ListOfStudents and Student class

---
+++ ListOfStudents.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class ListOfStudents {
+    public static void main(String[] args) {
+        List<Student> studentList = new ArrayList<>();
+        studentList.add(new Student("Yash",70));
+        studentList.add(new Student("Raj",72));
+        studentList.add(new Student("Rahul",81));
+        studentList.add(new Student("Het",90));
+        studentList.add(new Student("Sagar",61));
+        List<Student> filteredStudents=studentList.stream().filter((el)->el.getScore()>80).collect(Collectors.toList());
+        System.out.println(filteredStudents);
+    }
+}
