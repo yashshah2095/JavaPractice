719034c417251822644e2665d9aa6c6ae2eb4f85
systems <systems@SYNPUNLT2206009.int.asurion.com>
2022-06-29 18:26:19 +0530

    Added class to find smallest not present in array

---
+++ FindMinFromArray.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class FindMinFromArray {
+    public static void main(String[] args) {
+        int[] ar1 = {-1,-2,0,2,3};
+        Set<Integer> hmp1 = new HashSet();
+        for (int i : ar1) {
+            if (i > 0) {
+                hmp1.add(i);
+            }
+        }
+        int smallest = 0;
+        for (int j = 1; j <= ar1.length+1; j++) {
+            if (!hmp1.contains(j)) {
+                smallest = j;
+                break;
+            }
+        }
+        System.out.println(smallest);
+    }
+}
