bbee19450ea544827c33ae3e6a973103d6b32770
systems <systems@SYNPUNLT2206009.int.asurion.com>
2022-06-30 11:22:25 +0530

    Added class to group in RGB

--- RGBGrouping.java
+++ RGBGrouping.java
@@ -6,21 +6,16 @@ public class RGBGrouping {
     public static void main(String[] args) {
         String[] str1={"red","blue","green","red","blue","red","green"};
         ArrayList<String> str2=new ArrayList<>();
-        for(int i=0;i< str1.length;i++){
-            if(str1[i]=="red"){
-                str2.add(str1[i]);
-            }
-        }
-        for(int i=0;i< str1.length;i++){
-            if(str1[i]=="green"){
-                str2.add(str1[i]);
-            }
-        }
-        for(int i=0;i< str1.length;i++){
-            if(str1[i]=="blue"){
-                str2.add(str1[i]);
-            }
-        }
+        addElement("red",str2,str1);
+        addElement("green",str2,str1);
+        addElement("blue",str2,str1);
         System.out.println(str2);
+    }
+    public static void addElement(String color, ArrayList str2,String[] str1){
+        for(int i=0;i< str1.length;i++){
+            if(str1[i]==color){
+                str2.add(str1[i]);
+            }
+        }
     }
 }
